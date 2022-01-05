771cadfb38dce3e03cce0d8d7b11057e57e6a4c3
Matthew Singler <matthew.singler@gmail.com>
2022-01-05 14:33:25 -0600

    logout

--- src/Components/Nav/Navbar.js
+++ src/Components/Nav/Navbar.js
@@ -1,8 +1,9 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom"
 import "./Navbar.css"
 
 export const Navbar = () => {
+    const history = useHistory()
     return (
         <ul className="navbar">
             <li className="navbar__item">
