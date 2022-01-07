43d800817a9a6e6eb941ad4647256f2777f0b417
Matthew Singler <matthew.singler@gmail.com>
2022-01-07 11:38:25 -0600

    sty;ing background

---
+++ src/Components/Auth/AuthTheme.js
@@ -0,0 +1,9 @@
+import { createTheme } from '@mui/material/styles';
+import blue from '@material-ui/core/colors/blue'
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: blue[500]
+    },
+  },
+});
